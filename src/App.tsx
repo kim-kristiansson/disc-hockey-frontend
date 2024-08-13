cc5b14631dd50ff51b3db21e4d86122f6381b05c
kimkristianssonJU <krki21cn@student.ju.se>
2024-08-13 16:58:03 +0200

    feat: replace dummy block with MainHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import MainHeader from "./MainHeader/MainHeader";
+
 function App() {
 	return (
 		<>
-			<h1 className='text-4xl font-bold text-center text-blue-600'>
-				Hello Worlds!
-			</h1>
+			<MainHeader />
 		</>
 	);
 }
