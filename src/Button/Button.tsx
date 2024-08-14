e6bf5b65cdb687b40c983d3f6b04f7bdb833322a
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-14 13:39:53 +0200

    fix: change reusable button to use rem instead of px
    - Added type ButtonIconProps that omits IconProps without width, height, and children

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -2,8 +2,10 @@ import { ReactElement } from "react";
 import { IconProps } from "../icons/IconProps";
 import React from "react";
 
+type ButtonIconProps = Omit<IconProps, 'width' | 'height' | 'children'>
+
 interface ButtonProps {
-    icon?: ReactElement<IconProps>;
+    icon?: ReactElement<ButtonIconProps>;
     hidden?: boolean;
     ariaLabel: string;
     disabled?: boolean;
@@ -13,7 +15,7 @@ interface ButtonProps {
 
 const Button = ({icon, hidden, ariaLabel, disabled, onClick, text}: ButtonProps) => {
     const IconWithSize = icon 
-    ? React.cloneElement(icon, { width: 24, height: 24 }) 
+    ? React.cloneElement<IconProps>(icon, { width: '1.5rem', height: '1.5rem' }) 
     : null;
 
     return <button className="h-12" hidden={hidden} aria-label={ariaLabel} disabled={disabled} onClick={onClick}>{IconWithSize && <span>{IconWithSize}</span>}{text}</button>
