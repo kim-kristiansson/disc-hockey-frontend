5f97ef4212b7d56a43c5a9ee31bd458035018e08
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-14 15:16:51 +0200

    fix: add arial label but removed children

--- src/icons/IconProps.ts
+++ src/icons/IconProps.ts
@@ -1,11 +1,8 @@
-import { ReactNode } from "react";
-
 type RemUnit = `${number}rem`;
 
 export interface IconProps {
     width?: RemUnit;
     height?: RemUnit;
     color? : string;
-    children?: ReactNode;
-  }
-  
+    ariaLabel: string;
+}
