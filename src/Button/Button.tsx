e2ed0891457312d40d5953cde23ffee3bde38f54
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-14 15:18:52 +0200

    fix: change icon to ComponentType<IconProps>
    - Added iconProps

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,11 +1,11 @@
-import { ReactElement } from "react";
 import { IconProps } from "../icons/IconProps";
 import React from "react";
 
-type ButtonIconProps = Omit<IconProps, 'width' | 'height' | 'children'>
+type ButtonIconProps = Omit<IconProps, 'width' | 'height'>
 
 interface ButtonProps {
-    icon?: ReactElement<ButtonIconProps>;
+    iconProps?: ButtonIconProps;
+    icon?: React.ComponentType<IconProps>;
     hidden?: boolean;
     ariaLabel: string;
     disabled?: boolean;
@@ -13,10 +13,8 @@ interface ButtonProps {
     text: string;
 }
 
-const Button = ({icon, hidden, ariaLabel, disabled, onClick, text}: ButtonProps) => {
-    const IconWithSize = icon 
-    ? React.cloneElement<IconProps>(icon, { width: '1.5rem', height: '1.5rem' }) 
-    : null;
+const Button = ({iconProps, icon: Icon, hidden, ariaLabel, disabled, onClick, text}: ButtonProps) => {
+    const IconWithSize = Icon ? <Icon width="1.5rem" height="1.5rem" ariaLabel="Button Icon (No label specified)" {...iconProps} /> : null;
 
     return <button className="h-12" hidden={hidden} aria-label={ariaLabel} disabled={disabled} onClick={onClick}>{IconWithSize && <span>{IconWithSize}</span>}{text}</button>
 }
