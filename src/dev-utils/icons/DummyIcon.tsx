18a04e7f5495c95fbd1f6508899cd14004fc5f6a
kimkristianssonJU <krki21cn@student.ju.se>
2024-08-14 12:00:05 +0200

    chore: add DummyIcon for testing purposes

---
+++ src/dev-utils/icons/DummyIcon.tsx
@@ -0,0 +1,10 @@
+import BaseIcon from "../../icons/BaseIcon";
+import { IconProps } from "../../icons/IconProps";
+
+const DummyIcon = (props: IconProps) => (
+  <BaseIcon {...props}>
+    <rect />
+  </BaseIcon>
+);
+
+export default DummyIcon;
