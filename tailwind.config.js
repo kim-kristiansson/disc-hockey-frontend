205d38005b9a11d09a3e7f2669aab08752673cdc
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-15 07:54:49 +0200

    feat: add custom color to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            "spotify-green": {
+               DEFAULT: "#1DB954",
+               dark: "#1AA34A",
+            },
+         },
+      },
+   },
+   plugins: [],
+};
