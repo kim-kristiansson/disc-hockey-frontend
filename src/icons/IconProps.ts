56cc4d220b8f379351cb67fd3860ffc340e84785
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-15 07:54:25 +0200

    chore: formatting

--- src/icons/IconProps.ts
+++ src/icons/IconProps.ts
@@ -3,6 +3,6 @@ type RemUnit = `${number}rem`;
 export interface IconProps {
     width?: RemUnit;
     height?: RemUnit;
-    color? : string;
+    color?: string;
     ariaLabel: string;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		css: true,
-		setupFiles: "./vitest.setup.ts",
-	},
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        css: true,
+        setupFiles: "./vitest.setup.ts",
+    },
 });
