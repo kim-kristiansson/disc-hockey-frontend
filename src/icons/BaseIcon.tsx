301737c442ac1f57585e337c6cffd2ca4607c7b2
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-16 13:32:34 +0200

    feat: add dynamic margin based on size

--- src/icons/BaseIcon.tsx
+++ src/icons/BaseIcon.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import { IconProps } from './IconProps';
+import React from 'react'
+import { IconProps } from './IconProps.mts'
 
 interface BaseIconProps extends IconProps {
-    children: React.ReactNode;
+    children: React.ReactNode
 }
 
 const BaseIcon = ({
-  width = '4rem',
-  height = '4rem',
-  color = 'black',
-  children,
-  ariaLabel = 'Icon (Label not specified)'
+    width = '4rem',
+    height = '4rem',
+    color = 'black',
+    children,
+    ariaLabel = 'Icon (Label not specified)',
 }: BaseIconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={width}
-    height={height}
-    viewBox="0 0 64 64"
-    fill={color}
-    role='img'
-    style={{ width: width, height: height }}
-    aria-label={ariaLabel}
-  >
-    {children}
-  </svg>
-);
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width={width}
+        height={height}
+        viewBox='0 0 64 64'
+        fill={color}
+        role='img'
+        style={{
+            width: width,
+            height: height,
+            margin: `calc(${width} / 2) calc(${height} / 2)`,
+        }}
+        aria-label={ariaLabel}
+    >
+        {children}
+    </svg>
+)
 
-export default BaseIcon;
+export default BaseIcon
