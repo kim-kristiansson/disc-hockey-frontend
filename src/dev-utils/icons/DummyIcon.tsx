41346de41463c0bd96251b2cf9136fd3f98b847f
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-16 22:04:11 +0200

    chore: changed dummy icon

--- src/dev-utils/icons/DummyIcon.tsx
+++ src/dev-utils/icons/DummyIcon.tsx
@@ -2,8 +2,8 @@ import BaseIcon from '../../icons/BaseIcon'
 import { IconProps } from '../../icons/IconProps.mts'
 
 const DummyIcon = (props: IconProps) => (
-    <BaseIcon {...props}>
-        <rect />
+    <BaseIcon {...props} viewBox='0 0 512 512'>
+        <path d='M256 59.72L142.687 256h226.625L256 59.72zM369.313 256L256 452.28h226.625L369.312 256zM256 452.28L142.687 256 29.376 452.28H256z' />
     </BaseIcon>
 )
 
