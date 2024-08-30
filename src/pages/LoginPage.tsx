2d9dadfd5074e8af733b37bfd2f572d110a4068b
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-30 17:44:22 +0200

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,20 +3,20 @@ import MainHeader from '../components/MainHeader/MainHeader'
 import SpotifyIcon from '../dev-utils/icons/SpotifyIcon'
 
 const LoginPage = () => {
+    const handleOnClick = () => {
+        console.log('Button clicked!')
+    }
     return (
-        <>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
             <MainHeader />
-            <form onSubmit={() => console.log('submit')}>
-                <Button
-                    ariaLabel='Dummy Button'
-                    onClick={() => {
-                        console.log('Click!')
-                    }}
-                    text='Click Here'
-                    icon={SpotifyIcon}
-                />
-            </form>
-        </>
+            <Button
+                type='submit'
+                ariaLabel='Login Button'
+                text='Logga in med Spotify'
+                icon={SpotifyIcon}
+                onClick={handleOnClick}
+            />
+        </div>
     )
 }
 
