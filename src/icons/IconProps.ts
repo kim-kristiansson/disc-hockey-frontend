2f8557c8109d1a8d8650594fce53bc0fb0adcffd
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-31 08:34:54 +0200

    Create IconProps.ts

---
+++ src/icons/IconProps.ts
@@ -0,0 +1,9 @@
+type RemUnit = `${number}rem`
+
+export interface IconProps {
+    width?: RemUnit
+    height?: RemUnit
+    color?: string
+    ariaLabel: string
+    style?: React.CSSProperties
+}
