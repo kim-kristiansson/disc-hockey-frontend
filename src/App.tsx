34047f859098abd4cb16e75238db69fbeacc4c1f
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-21 19:20:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './features/auth/Login'
-import Callback from './components/Callback'
+import Dashboard from './features/dashboard/Dashboard'
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Login />} />
-                <Route path='/callback' element={<Callback />} />
+                <Route path='/dashboard' element={<Dashboard />} />
             </Routes>
         </Router>
     )
